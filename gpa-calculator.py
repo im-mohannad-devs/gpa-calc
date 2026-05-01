3013ea895610efd2a79691a29eb0da15b5fc8f9b
Mohanand Nader <mhndnadr2@gmail.com>
2026-05-01 17:04:53 +0300

    Refactor GPA calculator for improved readability

--- gpa-calculator.py
+++ gpa-calculator.py
@@ -1,5 +1,8 @@
-scale = input("Which scale do you want to calculate with?\nGlobal(G) scale or AOU(A) scale (G or A)\n=>").upper()
 
+print("========================================================================\n\t\t\tGPA Calculator\n========================================================================")
+
+scale = input("Which scale do you want to calculate with?\nGlobal(G) scale or AOU(A) scale (G or A)\n=>").upper()
+print("========================================================================")
 while scale not in ["G","A"]:
     print("Invalid Choice try again!")
     scale = input("Which scale do you want to calculate with?\nGlobal(G) scale or AOU(A) scale (G or A)\n=>").upper()
@@ -80,18 +83,18 @@ total_hours = 0
 total_points = 0
 
 for i in range(num):
-    mark = float(input(f"Enter the degree of course{i+1}:"))
+    mark = float(input(f"Enter the degree of course {i+1}:"))
     while mark < 0 or mark > 100:
         print("Invalid mark, try again")
-        mark = float(input(f"Enter the degree of course{i+1}:"))
-    hours = int(input(f"Enter credit hours of course{i+1}:"))
+        mark = float(input(f"Enter the degree of course {i+1}:"))
+    hours = int(input(f"Enter credit hours of course {i+1}:"))
     while hours <= 0:
         print("Invalid hours, try again")
-        hours = int(input(f"Enter credit hours of course{i+1}:"))
+        hours = int(input(f"Enter credit hours of course {i+1}:"))
     grade = get_grade(mark)
     points = grade_to_points(grade)
 
-    print(f"{i+1} Course Grade = {grade}, Points = {points}")
+    print(f"Course {i+1} Grade = {grade}, Points = {points}")
 
     total_hours += hours
     total_points += points * hours
